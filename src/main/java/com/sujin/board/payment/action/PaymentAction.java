a92b99f2a04ea79c6490c1771613712bc9f5b7dd
sujinhuh <sujin941220@gmail.com>
2023-07-16 20:26:23 +0900

    add comment - comment 저장 click하면 작동 오류 ,payment 기능구현 중

---
+++ src/main/java/com/sujin/board/payment/action/PaymentAction.java
@@ -0,0 +1,22 @@
+package com.sujin.board.payment.action;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class PaymentAction {
+    /***
+     *
+     get -> return string (page 이동)
+     post -> return response (api 호출)
+     */
+    @GetMapping("/payment")
+    public String payment(Model model) {
+
+        return "payment/payment";
+    }
+
+}
